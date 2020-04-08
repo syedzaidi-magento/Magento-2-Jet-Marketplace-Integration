b40dbb6a39bf291e1a12af8b855d3ac902cb3b46
syedzaidi-magento <syedqurat27@gmail.com>
2020-04-07 23:30:34 -0700

    Add admin menu and controller.

---
+++ Controller/Adminhtml/JetIntegrationController/Index.php
@@ -0,0 +1,50 @@
+<?php
+namespace Syedzaidi\JetIntegration\Controller\Adminhtml\JetIntegrationController;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'Syedzaidi_JetIntegration::catalog_products';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/jetintegration_jetintegrationcontroller_index.xml
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('Jet Products'));
+
+        return $resultPage;
+    }
+}
