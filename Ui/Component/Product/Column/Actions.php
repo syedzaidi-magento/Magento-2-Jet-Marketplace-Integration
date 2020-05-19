0d3b0f674723770ef4f624bda75b927d593d586a
syedzaidi-magento <syedqurat27@gmail.com>
2020-05-19 01:37:09 -0700

    Add action column in Product listing grid for view realtime single product data.

---
+++ Ui/Component/Product/Column/Actions.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace Syedzaidi\JetIntegration\Ui\Component\Product\Column;
+
+use Magento\Cms\Block\Adminhtml\Page\Grid\Renderer\Action\UrlBuilder;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class Actions extends Column
+{
+    /** Url path */
+    const PRODUCT_URL_PATH_VIEW = 'jetintegration/products/view';
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+    /**
+     * @var UrlBuilder
+     */
+    private $actionUrlBuilder;
+    /**
+     * @var string
+     */
+    private $editUrl;
+
+    /**
+     * Actions constructor.
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlBuilder $actionUrlBuilder
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     * @param string $editUrl
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlBuilder $actionUrlBuilder,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = [],
+        $editUrl = self::PRODUCT_URL_PATH_VIEW
+    ){
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+        $this->urlBuilder = $urlBuilder;
+        $this->actionUrlBuilder = $actionUrlBuilder;
+        $this->editUrl = $editUrl;
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $name = $this->getData('name');
+                if (isset($item['product_id'])) {
+                    $item[$name]['edit'] = [
+                        'href' => $this->urlBuilder->getUrl($this->editUrl, ['product_id' => $item['product_id']]),
+                        'label' => __('View'),
+                        '__disableTmpl' => true,
+                    ];
+                }
+                if (isset($item['identifier'])) {
+                    $item[$name]['preview'] = [
+                        'href' => $this->scopeUrlBuilder->getUrl(
+                            $item['identifier'],
+                            isset($item['_first_store_id']) ? $item['_first_store_id'] : null,
+                            isset($item['store_code']) ? $item['store_code'] : null
+                        ),
+                        'label' => __('View'),
+                        '__disableTmpl' => true,
+                    ];
+                }
+            }
+        }
+        return $dataSource;
+    }
+}
